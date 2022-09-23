dca3b64fc5c8cb51ed9f57d5dee1fcd79b95acfd
Derek Vance <dacrepair@gmail.com>
2022-09-23 15:22:30 -0400

    Cleaned some testing files that made it into the project.

--- test.py
+++
@@ -1,7 +0,0 @@
-from terraform_state_processor.terraform.tfstate import TerraformState
-
-with open('./tfstate.json') as fp:
-    fp.seek(0)
-    test = TerraformState(fp.read())
-    for x in test.resources:
-        print(x)
